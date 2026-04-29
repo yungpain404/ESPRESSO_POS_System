61843c866accea028ee4f93b81a16e348261fdac
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2026-04-29 11:02:32 +0700

    add menu management page

---
+++ src/app/Test.java
@@ -0,0 +1,21 @@
+package app;
+
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+public class Test{
+	public static void main(String[] args) {
+		try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(()->{
+//            new Login_UI().setVisible(true);
+            new MenuManagement().setVisible(true);
+        });
+	}
+}
