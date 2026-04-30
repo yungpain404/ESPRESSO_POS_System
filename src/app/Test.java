7714c2030e06c3ac5c81a0b25c3b5afb6449e7b7
Nguyen Mai Minh Quy <maiminhquyn@gmail.com>
2026-04-30 15:29:42 +0700

    save for currnet changes

--- src/app/Test.java
+++ src/app/Test.java
@@ -19,7 +19,7 @@ public class Test{
 
         SwingUtilities.invokeLater(()->{
             new LoginForm().setVisible(true);
-//        	new MenuManagement().setVisible(true);
+
         });
         
         
