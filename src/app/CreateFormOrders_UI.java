a217419a33c5ef76c5095cc1bb96cc09fac2903b
Nguyễn Ngọc Hậu <nguyenngochau092@gmail.com>
2026-05-05 16:50:41 +0700

    Valid data

--- src/app/CreateFormOrders_UI.java
+++ src/app/CreateFormOrders_UI.java
@@ -5,7 +5,6 @@ import com.formdev.flatlaf.FlatLightLaf;
 
 import dao.HoaDon_DAO;
 import dao.Mon_DAO;
-import dao.TaiKhoan_DAO;
 import entity.ChiTietHoaDon;
 import entity.HoaDon;
 import entity.Mon;
@@ -755,8 +754,8 @@ public class CreateFormOrders_UI extends JFrame implements ActionListener {
     }
     public void renderMenu() {
         pnlProductGrid.removeAll();
-        java.util.List<entity.Mon> listMon = mon_dao.getAll();
-        for (entity.Mon m : listMon) {
+        List<Mon> listMon = mon_dao.getAll();
+        for (Mon m : listMon) {
             JPanel card = createProductCard(
                 m.getMaMon(), m.getTenMon(), m.getMoTaMon(),
                 String.format("$%.2f", m.getDonGiaBan()), 
@@ -776,7 +775,7 @@ public class CreateFormOrders_UI extends JFrame implements ActionListener {
         System.out.println(originalList);
 
         if (originalList != null) {
-            for (entity.Mon m : originalList) {
+            for (Mon m : originalList) {
                 JPanel card = createProductCard(
                     m.getMaMon(),
                     m.getTenMon(), 
@@ -845,7 +844,7 @@ public class CreateFormOrders_UI extends JFrame implements ActionListener {
     }
     private void displayFilteredList(java.util.List<entity.Mon> list) {
         pnlProductGrid.removeAll();
-        for (entity.Mon m : list) {
+        for (Mon m : list) {
             JPanel card = createProductCard(
                 m.getMaMon(), m.getTenMon(), m.getMoTaMon(),
                 String.format("$%.2f", m.getDonGiaBan()), 
